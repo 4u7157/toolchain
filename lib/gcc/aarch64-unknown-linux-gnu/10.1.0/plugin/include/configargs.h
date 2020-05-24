0916043f825d7aa4836fb2a6b423f1bfb58a107a
root <root@semaphore-vm.vm>
2020-05-24 08:49:00 +0000

    gcc 10.1.0 toolchain cortex-a53

---
+++ lib/gcc/aarch64-unknown-linux-gnu/10.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/semaphore/crosstool-ng/.build/aarch64-unknown-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-unknown-linux-gnu --prefix=/home/semaphore/x-tools/aarch64-unknown-linux-gnu --with-sysroot=/home/semaphore/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-languages=c,c++ --with-cpu=cortex-a53 --with-pkgversion='crosstool-NG UNKNOWN' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/home/semaphore/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --with-mpfr=/home/semaphore/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --with-mpc=/home/semaphore/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --with-isl=/home/semaphore/crosstool-ng/.build/aarch64-unknown-linux-gnu/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/semaphore/x-tools/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a53" } };
